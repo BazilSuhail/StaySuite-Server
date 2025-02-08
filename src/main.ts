a65b826794ea9d6c30fedfb531065489332fd111
BazilSuhail <bazil1854@gmail.com>
2025-02-08 14:37:00 +0500

    Updated CORS origin access for deployment

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS
-  app.enableCors({
+  /*app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173' || 'http://localhost:3000', // Use environment variable for flexibility
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allow cookies and credentials
+  });*/
+  app.enableCors({
+    origin: '*', // Allow access from any origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true, // Note: Credentials do not work with '*', consider using a specific list or `true` with `Access-Control-Allow-Origin`
   });
+
 
   //app.use('/listing_images', express.static(path.join(__dirname, '..', 'listing_images')));
 
